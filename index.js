bafc0626dd587aca6b850e670a851562431c6214
Chimatara Kalu <taradaniel05@gmail.com>
2023-02-27 18:08:39 -0800

    Modification

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// QUESTION 1
+
+ const instagram = new Object();
+ instagram.author = 'Chimatara';
+ instagram.content = 'Baking Shop';
+ instagram.imagelink = 'https://www.instagram.com';
+ instagram.views = '100 views';
+ instagram.likes = '1024 likes';
+
+console.log(instagram)
+
+function instagramPost(author, content,imagelink,views,likes) {
+   this.author = author;
+   this.content = content;
+   this.imagelink = imagelink;
+   this.views = views;
+   this.likes = likes;
+}
+
+
+// QUESTION 2
+
+const myFirstPost = new instagramPost('Benita', 'Dance Video', 'https://tara.com', 100, '5K likes')
+console.log(myFirstPost)
+
+const mySecondPost = new instagramPost('Korty', 'Comedy', 'https://.com', 100, '5K likes')
+
 // QUESTION 3
 
       //    (a)
